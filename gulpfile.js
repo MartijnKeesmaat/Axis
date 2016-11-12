dc827757865c224746cccedb529f228f3bcfca36
Martijn Keesmaat <martijnk@tjuna.com>
2016-11-12 20:51:09 +0100

    Added prefixer to gulpflie

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require("gulp"),
 browserSync = require('browser-sync').create();
 sass = require("gulp-sass");
 concat = require('gulp-concat');
+const autoprefixer = require('gulp-autoprefixer');
 
 function errorLog(error){
   console.error.bind(error);
@@ -15,6 +16,7 @@ gulp.task('serve', ['sass'], function() {
   });
 
   gulp.watch("app/styles/**/*.scss", ['sass']);
+  gulp.watch("app/scripts/*.js", ['concat']).on('change', browserSync.reload);
   gulp.watch("app/*.html").on('change', browserSync.reload);
 });
 
@@ -25,10 +27,14 @@ gulp.task('sass', function() {
     .pipe(browserSync.stream());
 });
 
-// Watch tasks
-gulp.task("watch", function(){
-  gulp.watch('app/scripts/*.js', ["concat"]);
-});
+gulp.task('prefix', () =>
+  gulp.src('app/styles/main.css')
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
+    .pipe(gulp.dest('app/styles'))
+);
 
 // Concat
 gulp.task('concat', function(){
@@ -38,4 +44,5 @@ gulp.task('concat', function(){
 });
 
 
-gulp.task("default", ['watch', 'concat', 'serve']);
+// gulp.task("default", ['watch', 'concat', 'serve']);
+gulp.task("deploy", ['prefix']);
