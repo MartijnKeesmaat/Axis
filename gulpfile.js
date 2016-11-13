c01e8bf5a7b5fd5a065897b0f9d5ec90ac9f4c2b
Martijn Keesmaat <martijnk@tjuna.com>
2016-11-13 23:32:24 +0100

    Gulp testing

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,41 @@
+// Require dependencies
 var gulp = require("gulp"),
 browserSync = require('browser-sync').create();
 sass = require("gulp-sass");
 concat = require('gulp-concat');
 const autoprefixer = require('gulp-autoprefixer');
 var csso = require('gulp-csso');
+var plumber = require('gulp-plumber');
+var strip = require('gulp-strip-comments');
 
+
+
+// Error message
 function errorLog(error){
   console.error.bind(error);
   this.emit('end');
 }
 
-gulp.task('serve', ['sass'], function() {
 
-  browserSync.init({
-      server: "./app"
-  });
-
-  gulp.watch("app/styles/**/*.scss", ['sass']);
-  gulp.watch("app/scripts/*.js", ['concat']).on('change', browserSync.reload);
-  gulp.watch("app/*.html").on('change', browserSync.reload);
+// Strip comments
+gulp.task('default', function () {
+  return gulp.src('template.js')
+    .pipe(strip())
+    .pipe(gulp.dest('dist'));
 });
 
+
+// Sass
 gulp.task('sass', function() {
   return gulp.src("app/styles/**/*.scss")
+    .pipe(plumber())
     .pipe(sass())
     .pipe(gulp.dest("app/styles"))
     .pipe(browserSync.stream());
 });
 
+
+// Css prefix
 gulp.task('prefix', () =>
   gulp.src('app/styles/main.css')
     .pipe(autoprefixer({
@@ -37,17 +45,33 @@ gulp.task('prefix', () =>
     .pipe(gulp.dest('app/styles'))
 );
 
+
+// Css minify
 gulp.task('minify', function () {
   return gulp.src('app/styles/main.css')
     .pipe(csso())
     .pipe(gulp.dest('app/styles'));
 });
 
+
 // Concat
 gulp.task('concat', function(){
   return gulp.src('app/scripts/*.js')
     .pipe(concat('main.js'))
     .pipe(gulp.dest('app/scripts/dist'));
+});
+
+
+// Serve
+gulp.task('serve', ['sass'], function() {
+
+  browserSync.init({
+      server: "./app"
+  });
+
+  gulp.watch("app/styles/**/*.scss", ['sass']);
+  gulp.watch("app/scripts/*.js", ['concat']).on('change', browserSync.reload);
+  gulp.watch("app/*.html").on('change', browserSync.reload);
 });
 
 
