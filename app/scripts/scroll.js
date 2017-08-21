d1771e2035725e5ce2d38c3f0ec1491fd20b34de
Martijn Keesmaat <martijnk@tjuna.com>
2017-08-21 09:21:11 +0200

    removed duplicate scroll script

--- app/scripts/scroll.js
+++ app/scripts/scroll.js
@@ -84,35 +84,3 @@ $('a[href*="#"]')
   $(function(){
     $('[data-scroll-speed]').moveIt();
   });
-
-
-
-  $.fn.moveIt = function(){
-    var $window = $(window);
-    var instances = [];
-
-    $(this).each(function(){
-      instances.push(new moveItItem($(this)));
-    });
-
-    window.onscroll = function(){
-      var scrollTop = $window.scrollTop();
-      instances.forEach(function(inst){
-        inst.update(scrollTop);
-      });
-    }
-  }
-
-  var moveItItem = function(el){
-    this.el = $(el);
-    this.speed = parseInt(this.el.attr('data-scroll-speed'));
-  };
-
-  moveItItem.prototype.update = function(scrollTop){
-    this.el.css('transform', 'translateY(' + -(scrollTop / this.speed) + 'px)');
-  };
-
-  // Initialization
-  $(function(){
-    $('[data-scroll-speed]').moveIt();
-  });
